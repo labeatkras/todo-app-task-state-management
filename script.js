5317e20bd28bbd4a73c710cc7041b315cf7d5ae8
Labeat Krasniqi <labeat.kras@gmail.com>
2023-05-19 12:47:40 +0200

    Start new todo

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // 1. Liste anzeigen
 // 2. Neue Einträge hinzufügen
 // 3. Bestehende Einträge löschen
+const list = document.querySelector("ul");
 
 const todos = ["Eis essen", "Einkaufen", "Nico huldigen"];
 
-function render() {
+function renderTodos() {
   // todo: render all todos in the ul
+  const newLI = document.createElement("li");
+  const todoText = document.createElement("todos");
+  newLI.append(todoText);
+
+  list.append(newLi);
 }
 
 render();
